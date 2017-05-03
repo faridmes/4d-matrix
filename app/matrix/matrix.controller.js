432bf876e22805da6bf6ff1961863db9f3d19f9f
lamrii <faridm.dev@gmail.com>
2017-05-04 00:34:14 +0200

    Create matrix and add form to add task in it.

--- app/matrix/matrix.controller.js
+++ app/matrix/matrix.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular.module('4d-matrix.matrix')
@@ -7,7 +7,30 @@
     function MatrixController() {
         var vm = this;
 
-        vm.hello = 'hello';
+        vm.doList = [];
+        vm.delegateList = [];
+        vm.decideList = [];
+        vm.deleteList = [];
+        vm.newTask = '';
+        vm.category = '';
+
+        vm.addTask = function () {
+            switch (vm.category) {
+                case 'do':
+                    vm.doList.push(vm.newTask);
+                    break;
+                case 'delegate':
+                    vm.delegateList.push(vm.newTask);
+                    break;
+                case 'decide':
+                    vm.decideList.push(vm.newTask);
+                    break;
+                case 'delete':
+                    vm.deleteList.push(vm.newTask);
+                    break;
+                default:
+            }
+        }
     }
 
 }());
